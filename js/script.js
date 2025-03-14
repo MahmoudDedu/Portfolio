f8f2cf2eb970cf61cfe47c3ec2e27ad52d96ba8f
MahmoudDedu <mahmouddarwishedu@gmail.com>
2025-03-15 02:26:14 +0300

    fotter and button to top

--- js/script.js
+++ js/script.js
@@ -69,3 +69,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTop = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+
+  backToTop.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
+
